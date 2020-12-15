1becaafd9a0571542b731f8394634acc43d77f09
Christian Rodriguez <cisr918@gmail.com>
2020-12-15 18:17:37 -0500

    Add more projects

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
 $(document).ready(function() {
 
     /** Dropdown menus **/
-    
+
+    /*function getPageIndex(elem) {
+        const elemIndex = elem.index();
+        let i = -1;
+        $("#pages").children(".dropdown").each(function(index, e) {
+            if ($(e).index() === elemIndex) {
+                i = index;
+                return false; // break
+            }
+        });
+        return i;
+    }*/
+
     $(".dropdown > a").mouseenter(function() {
-        setStyle($(this), "#f52549", "bold");
+        const self = $(this);
+        setTransition(self, 1.09, "ease-in");
+        setFontStyle(self, "f52549", "bold");
     });
     $(".dropdown > a").mouseleave(function() {
-        setStyle($(this), "#f2f2f2", "normal");
+        const self = $(this);
+        setTransition(self, 1, "ease-out");
+        setFontStyle(self, "f2f2f2", "normal");
     });
     $(".dropdown-content").mouseenter(function() {
-        setStyle($(this).siblings("a"), "#f52549", "bold");
+        const sibling = $(this).siblings("a");
+        setTransition(sibling, 1.09, "ease-in");
+        setFontStyle(sibling, "f52549", "bold");
     });
     $(".dropdown-content").mouseleave(function() {
-        setStyle($(this).siblings("a"), "#f2f2f2", "normal");
+        const sibling = $(this).siblings("a");
+        setTransition(sibling, 1, "ease-out");
+        setFontStyle(sibling, "f2f2f2", "normal");
     });
 
     $(".dropdown-content").children("a").each(function(index, elem) {
         $(elem).css("transition-delay", `${index * 0.03}s`);
     });
 
-    function setStyle(elem, color, fontWeight) {
+    function setTransition(elem, scale, timing) {
+        elem.css({
+            transform: `scale(${scale})`,
+            transition: "transform 0.1s " + timing
+        });
+    }
+
+    function setFontStyle(elem, color, fontWeight) {
         if (elem.hasClass("location"))
             return;
         elem.css({
-            color: color,
+            color: '#' + color,
             fontWeight: fontWeight
         });
     }
+
+    /*function rand(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function generateStars(count) {
+        let str = "";
+        const width = $(document).width();
+        const height = $(document).height();
+        for (let i = 0; i < count; ++i) {
+            str += `${rand(0, width)}px ${rand(0, height)}px #FFF,`;
+        }
+        return str;
+    }
+
+    console.log(generateStars(700));
+    console.log(generateStars(200));
+    console.log(generateStars(100));*/
 });
